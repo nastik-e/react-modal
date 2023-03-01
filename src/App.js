2fa3ba9e4d7c8247c786a585944f6793ea195901
nastik-e <105751995+nastik-e@users.noreply.github.com>
2023-03-02 00:07:29 +0300

    if-rendering modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setOpen(true)}>✨ Click me</button>
+      {open && (<div className="overlay">
+        <div className="modal">
+          <div onClick={()=> setOpen(false)} className="closeBtn">X</div>
+          <img
+            className="image"
+            src="https://media.tenor.com/Rme4nOJ8P7IAAAAM/%E1%83%94%E1%83%98%E1%83%A4%E1%83%9D%E1%83%A0%E1%83%98%E1%83%90-%E1%83%AE%E1%83%90%E1%83%97%E1%83%A3%E1%83%9C%E1%83%90.gif"
+          />
+        </div>
+      </div>)}
     </div>
   );
 }
